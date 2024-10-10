93056eabd5fc08869510fc71e94a4b6b2a24f840
maksis <maksis@adrenaline-network.com>
2024-10-10 12:41:42 +0300

    libnatpmp: install CMake config files

--- ports/libnatpmp/portfile.cmake
+++ ports/libnatpmp/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 8257134a5dcb077e40db1946554d676e444406e4
     SHA512 eb277a2507d658d5f5b8af4af44b80eabdd9ea460c498f736c2e6417dd1026994916cdafb0aaf31c0687cec38abe6abfd9179a5afe2c3c87d2203564db88b97a
     HEAD_REF master
+    PATCHES
+      fix-vcpkg-install.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIBS)
@@ -21,6 +23,8 @@ vcpkg_copy_pdbs()
 # vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/natpmp PACKAGE_NAME natpmp)
 
 vcpkg_fixup_pkgconfig()
+# vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/natpmp)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
